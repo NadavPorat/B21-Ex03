110dcdded8148e2555766e7572d99f136e215e16
Nadav Porat <nadavpo@mta.ac.il>
2021-05-17 11:56:18 +0300

    create get vehicle typr

--- Ex03.ConsoleUI/UI.cs
+++ Ex03.ConsoleUI/UI.cs
@@ -7,12 +7,60 @@ using Ex03.GarageLogic;
 
 namespace Ex03.ConsoleUI
 {
+
     public class UI
     {
+
         public Creator.EVehicleType GetVehicleType()
         {
-            return Creator.EVehicleType.ElectricMotor;
+            string choice;
+            Console.WriteLine("Plase Choes Vehicle Type: ");
+            Console.WriteLine();
+            Console.WriteLine("1. Gas Car");
+            Console.WriteLine("2. Electric Car");
+            Console.WriteLine("3. Gas Motorcycle");
+            Console.WriteLine("4. Electric Motorcycle");
+            Console.WriteLine("5. Truk");
+            choice = Console.ReadLine();
+            int Numchoice = Int32.Parse(choice);
+
+            while (Numchoice < 1 || Numchoice > 5)
+            {
+                Console.WriteLine("Eror,Plase Enter Agine: ");
+                choice = Console.ReadLine();
+                Numchoice = Int32.Parse(choice);
+            }
+
+
+            Creator.EVehicleType eCarType = 0;
+
+            switch (Numchoice)
+            {
+                case 1:
+                    eCarType = Creator.EVehicleType.GasCar;
+                    break;
+                case 2:
+                    eCarType = Creator.EVehicleType.ElectricCar;
+                    break;
+                case 3:
+                    eCarType = Creator.EVehicleType.GasMotor;
+                    break;
+                case 4:
+                    eCarType = Creator.EVehicleType.ElectricMotor;
+                    break;
+                case 5:
+                    eCarType = Creator.EVehicleType.Truck;
+                    break;
+            }
+            return eCarType;
+
         }
+
+        public void GetVehicleDetailes()
+        {
+
+        }
+
 
         public string GetModelName()
         {
@@ -25,3 +73,5 @@ namespace Ex03.ConsoleUI
         }
     }
 }
+
+
